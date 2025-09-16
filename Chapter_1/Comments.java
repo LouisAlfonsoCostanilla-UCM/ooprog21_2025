77754f7080df7e5efe13f13070128a5b83e9ea60
LouisAlfonsoCostanilla-UCM <costanilla.louis@gmail.com>
2025-09-16 08:49:44 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,22 @@
+/**
+3 COMMENT TYPES IN JAVA
+ */
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single line comment.
+        System.out.println("Single-line comment above this line.");
+
+        /*
+         * This is a multi-line comment.
+         * It can be used for long comments.
+         */
+        System.out.println("Multi-line comment above this line.");
+
+        /**
+         This is a documentation comment. It's used to describe methods
+         and classes in your code.
+         */
+        System.out.println("Documentation comment above this line.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("They've been sayin'");
+        System.out.println("You're sophisticated");
+        System.out.println("You are saying all the words I'm dreaming");
+        System.out.println("Say it after me");
+        
+        
+    }
+}
